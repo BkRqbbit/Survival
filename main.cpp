4d5eb303f15b48779e8cfaa7f2cc395fc7ed046d
BkRqbbit <bladyalvarez115dev@gmail.com>
2022-07-18 14:57:29 -0500

    Create the main window

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<SFML/Graphics.hpp>
+
+int main(){
+    sf::VideoMode windowDimensions(1280, 720);
+    std::string windowTitle = "Survival - The End Of Our Times";
+    sf::RenderWindow window( windowDimensions, windowTitle , sf::Style::Titlebar | sf::Style::Close);
+    return 0;
+}
