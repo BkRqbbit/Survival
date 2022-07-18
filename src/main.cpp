cd14fdf79328eb119ace9675ca8fe8c0b0e831e9
BkRqbbit <bladyalvarez115dev@gmail.com>
2022-07-18 16:01:14 -0500

    Add window event handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,13 @@ int main(){
     sf::VideoMode windowDimensions(1280, 720);
     std::string windowTitle = "Survival - The End Of Our Times";
     sf::RenderWindow window( windowDimensions, windowTitle , sf::Style::Titlebar | sf::Style::Close);
+
+    while( window.isOpen() ){
+        sf::Event event;
+        while( window.pollEvent( event ) ){
+            if( event.type == sf::Event::Closed )
+                window.close();
+        }
+    }
     return 0;
 }
